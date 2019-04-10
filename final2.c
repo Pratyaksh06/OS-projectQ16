6b0c58a4603ea90e6e4f7a9aa50301551d096e65
Pratyaksh06 <43118103+Pratyaksh06@users.noreply.github.com>
2019-04-10 19:57:53 +0530

    Update final2.c

--- final2.c
+++ final2.c
@@ -1,89 +1,87 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-// the structure for round robin scheduling algorithm with gantt chart.
-
- struct struct_rra
+ #include<stdio.h>
+int main()
 {
-int process_id, burst_time, burst_time_bal,arrival_time;
-float total_average_time, waiting_time;
-};
-int main()  
-  {
-  struct struct_rra process[10];
-  int i,j,k,n,tq;
-  int sum_burst_time=0,sum_total_average_time=0,sum_waiting_time=0,sum_total_arrival_time=0,tq_used=0;
-  int gantt[2][50];//gant chart 2d array
-  printf("Enter number of process required : ");
-  scanf("%d",&n);
-  for(i=0;i<n;i++) 
-  {
-  	printf("\n\nArrival Time:\t");
-    scanf("%d", &process[i].arrival_time);
-    printf("\n\nBurst-time of process %d: ",i+1);
-    process[i].process_id = i+1; //giving process id
-    scanf("%d",&process[i].burst_time);
-    process[i].burst_time_bal = process[i].burst_time;
-  }
-  printf("\n\nEnter time quantums of the process : ");
-  scanf("%d",&tq);
-  for( i=0;i<n;i++)
-  sum_burst_time += process[i].burst_time;
-  printf("\nSum of burst time = %d\n",sum_burst_time);
-  k=0;
-  do
-  {
-    for( i=0;i<n;i++) {
-      if( process[i].burst_time_bal > 0 && process[i].burst_time_bal<= tq )
+     int k,j,q,i,n,time_slice,temp;
+     int Total_wait;        float average_waiting_time;
+     int burst_time[10],wait_time[10],te[10],rt[10],arrival_time[10];j=0;
+     printf("enter number of process :\t");
+     scanf("%d",&n);
+    printf("\n enter brust time and arriavl time\n");
+    for(i=0;i<n;i++)
       {
-        tq_used += process[i].burst_time_bal;
-        process[i].total_average_time = tq_used;
-        process[i].waiting_time = process[i].total_average_time - process[i].burst_time;
-        process[i].burst_time_bal = 0;
-        gantt[0][k] = process[i].process_id;
-        gantt[1][k] = tq_used;
-        k++;
+          printf("P%d  \nbrust time ",i+1);
+          scanf("%d",&burst_time[i]);
+          printf("ariavl time   :  ");
+          scanf("%d",&arrival_time[i]);
+          te[i]=0;     wait_time[i]=0;
       }
-      else if( process[i].burst_time_bal >0 )
+    for(i=0;i<n;i++)
       {
-        tq_used += tq;
-        process[i].burst_time_bal -= tq;
-        gantt[0][k] = process[i].process_id;
-        gantt[1][k] = tq_used;
-        k++;
+        for(j=i+1;j<n;j++)
+         {
+             if(arrival_time[i]>arrival_time[j])
+               {
+                    temp=arrival_time[i];                                          //here i am sorting the processes according to their arrival time
+                   	 	arrival_time[i]=arrival_time[j];
+                    		arrival_time[j]=temp;
+                    			temp=burst_time[i];
+                    				burst_time[i]=burst_time[j];
+					                    burst_time[j]=temp;
+              }
+         }
+     }
+    printf("\n enter time slice\t");
+    scanf("%d",&time_slice); q=0;   
+    printf("\nprocess      :")  ;
+    for(i=0;i<n;i++)
+     {
+       printf("  %d",i+1);
       }
-      else if( process[i].burst_time_bal < 0 )
+    printf("\nBrust time   :");
+    for(i=0;i<n;i++)
+       {
+         printf("  %d",burst_time[i]); rt[i]=burst_time[i];
+        }
+    printf("\nArrival time :");
+    for(i=0;i<n;i++)
       {
-        printf("\nError: burst time Negative \n");
-        
+          printf("  %d",arrival_time[i]);
       }
-  }} while( tq_used != sum_burst_time);
+    printf("\n Gaint chart \n");
+    j=0;
 
-
-printf("\n Round Robin scheduling program in c with gantt chart\n\n");
-printf("process_id: ");
-for( i=0; i<n;i++)
-printf("\t%d",gantt[0][i]);
-printf("\n\nTime: ");
-for( i=0; i<n;i++)
-printf("\t%d",gantt[1][i]);
-for( i=0;i<n;i++)
-sum_waiting_time += process[i].waiting_time;
-for( i=0;i<n;i++)
-sum_total_average_time += process[i].total_average_time;
-printf("\n\nProcess_id: ");
-for( i=0;i<n;i++)
-printf("\t%d",i+1);
-printf("\n\nArrival Time: ");
-for( i=0;i<n;i++)
-printf("\t%d",process[i].arrival_time);
-printf("\n\nBurst_time: ");
-for( i=0;i<n;i++)
-printf("\t%d",process[i].burst_time);
-
-printf("\n\nTotal waiting time = %d\n",sum_waiting_time);
-printf("Average waiting time = %.2f\n",(float)sum_waiting_time/n);
-printf("\n\nTotal turn around time = %d\n",sum_total_average_time);
-printf("Average turn around time = %.2f\n\n",(float)sum_total_average_time/n);
-return 0;
-}
+    while(j<=n)
+       {
+          j++;
+          for(i=0;i<n;i++)
+            {
+              if(rt[i]==0)  continue;
+                   if(rt[i]>time_slice)
+                     {
+                        printf("\n %d\t P%d",q,i+1);
+                        q=q+time_slice;
+                        rt[i]=rt[i]-time_slice;
+                        te[i]=te[i]+1;
+                     }
+                  else
+                    {
+                       printf("\n %d\t P%d",q,i+1);
+                       wait_time[i]=q-te[i]*time_slice;
+                       q=q+rt[i]; 
+                       rt[i]=rt[i]-rt[i];
+                    }
+            }
+       }                                   //end of while     
+    average_waiting_time=0;
+    printf("\n Process   Waitnig time");
+    for(i=0;i<n;i++)
+       {
+                wait_time[i]=wait_time[i]-arrival_time[i];
+                    printf("\n P%d      :   %d",i+1,wait_time[i]); 
+					   	average_waiting_time=average_waiting_time+wait_time[i];
+        }
+    Total_wait=average_waiting_time;
+    printf("\ntotal waiting time %d",Total_wait);
+    printf("\n Avg wainting time %f ",average_waiting_time/n);
+    
+ }
